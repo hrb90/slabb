4774b7504b0d416e63ca322c92d25a7a786b734f
hrb90 <harrisonrbrown90@gmail.com>
2017-02-15 10:47:26 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+jeff = User.create({username: "jwu", password: "jeffwutangclan"})
+Channel.destroy_all
+channels = Channel.create([{name: "general", description: "Team-wide announcements and discussion", channel_type: "channel"},
+  {name: "random", description: "Off-topic discussion and silliness", channel_type: "channel"}])
+jeff_self_dm = Channel.create({name: "Jeff Self DM", channel_type: "dm", dm_user_ids: [jeff.id]})
