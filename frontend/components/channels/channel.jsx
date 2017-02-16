a2fd685fe1a747d74bd63446af827381b2c40a66
hrb90 <harrisonrbrown90@gmail.com>
2017-02-16 18:06:28 -0500

    Add subscribe button to placeholder Channel

--- frontend/components/channels/channel.jsx
+++ frontend/components/channels/channel.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { unsubscribeFromChannel } from '../../actions/channel_actions';
+import { subscribeToChannel, unsubscribeFromChannel } from '../../actions/channel_actions';
+import { fixDMName } from '../../util/channel_util';
 
-const mapStateToProps = ({currentChannel}) => ({
+const mapStateToProps = ({currentChannel, session}) => ({
   id: currentChannel.id,
-  name: currentChannel.name,
+  name: (currentChannel.channel_type !== "dm") ? currentChannel.name : fixDMName(currentChannel.name, session.currentUser.username),
   topic: currentChannel.topic
 });
 
 const mapDispatchToProps = dispatch => ({
-  unsubscribe: id => () => dispatch(unsubscribeFromChannel(id))
+  unsubscribe: id => () => dispatch(unsubscribeFromChannel(id)),
+  subscribe: id => () => dispatch(subscribeToChannel(id))
 })
 
 const Channel = (props) => (
   <div>
     <span>{ props.name }</span>
     <span>{ props.topic }</span>
+    <button onClick={ props.subscribe(props.id) }>Subscribe</button>
     <button onClick={ props.unsubscribe(props.id) }>Unsubscribe</button>
   </div>
 );
