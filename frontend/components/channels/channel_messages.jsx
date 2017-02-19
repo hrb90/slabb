c5699ad0af5456f43e25436fd4a3fb876a094878
hrb90 <harrisonrbrown90@gmail.com>
2017-02-19 17:37:24 -0500

    Group messages

--- frontend/components/channels/channel_messages.jsx
+++ frontend/components/channels/channel_messages.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Message from './messages/message';
-import { range } from 'lodash';
 
 class ChannelMessages extends React.Component {
+  firstMsgIds(messages) {
+    let firstMsgIds = [];
+    if (messages.length > 0) {
+      firstMsgIds.push(messages[0].id);
+      let currentAuthorId = messages[0].author.id;
+      messages.forEach(message => {
+        if (message.author.id !== currentAuthorId) {
+          firstMsgIds.push(message.id);
+          currentAuthorId = message.author.id;
+        }
+      });
+    }
+    return firstMsgIds;
+  }
+
   render() {
+    let firstMsgIds = this.firstMsgIds(this.props.messages);
     return (
       <div className="msgs-container">
         { this.props.messages.map(msg =>
-          (<Message author={ msg.author.username }
+          (<Message author={ msg.author }
             key={ msg.id }
-            content={ msg.content } />))}
+            content={ msg.content }
+            isFirst = { firstMsgIds.includes(msg.id) }/>))}
       </div>
     );
   }
--- frontend/components/channels/messages/message.jsx
+++ frontend/components/channels/messages/message.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Message = ({author, content}) => (
-  <div className="message">
-    <strong>{ author }</strong>: <p>{ content }</p>
-  </div>
-);
+const Message = ({author, content, timestamp, isFirst}) => {
+  if (isFirst) {
+    return (
+      <div className="message first">
+        <img className="avatar"
+          src="http://appacademy.github.io/css-friends/shared/img/cat.jpg"></img>
+        <div className="message-container">
+          <p className="author-name">{ author.username }</p>
+          <p className="msg-content">{ content }</p>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="message">
+        <p className="msg-content">{ content }</p>
+      </div>
+    )
+  }
+}
 
 export default Message;
