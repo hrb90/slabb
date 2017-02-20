51ee045bd225ef0cf6679dcc13e4755849f8758b
hrb90 <harrisonrbrown90@gmail.com>
2017-02-19 21:09:13 -0500

    Group messages by timestamp as well as author

--- frontend/components/channels/channel_messages.jsx
+++ frontend/components/channels/channel_messages.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Message from './messages/message';
 
+const MAX_GROUP_TIME_DIFF = 30 * 60 * 1000; // 30 minutes in milliseconds
+
 class ChannelMessages extends React.Component {
   firstMsgIds(messages) {
     let firstMsgIds = [];
     if (messages.length > 0) {
       firstMsgIds.push(messages[0].id);
       let currentAuthorId = messages[0].author.id;
+      let currentCreatedDate = new Date(messages[0].created_at);
       messages.forEach(message => {
-        if (message.author.id !== currentAuthorId) {
+        let msgDate = new Date(message.created_at);
+        if (message.author.id !== currentAuthorId ||
+            msgDate - currentCreatedDate > MAX_GROUP_TIME_DIFF) {
           firstMsgIds.push(message.id);
           currentAuthorId = message.author.id;
+          currentCreatedDate = msgDate;
         }
       });
     }
