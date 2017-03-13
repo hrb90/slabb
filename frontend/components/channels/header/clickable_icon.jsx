355fd4f543ec234c36d395a2dc81d070c299f54d
hrb90 <harrisonrbrown90@gmail.com>
2017-03-12 23:35:13 -0400

    Add tooltips to clickable icon

--- frontend/components/channels/header/clickable_icon.jsx
+++ frontend/components/channels/header/clickable_icon.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 const ClickableIcon = ({ faName, altText, afterText, onClick}) => (
   <div className="clickable-icon"
     onClick={ onClick }>
     <i className={`fa ${faName}`}
-      alt={ altText }
+      data-tip={ altText }
       aria-hidden="true"></i>
     { afterText }
   </div>
