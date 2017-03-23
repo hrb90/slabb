1d5c5733238a2989d8d028a3b5c89b1b7d314f19
hrb90 <harrisonrbrown90@gmail.com>
2017-03-22 22:18:28 -0400

    extract methods in navlistitem

--- frontend/components/nav_bar/nav_list_item.jsx
+++ frontend/components/nav_bar/nav_list_item.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 
-const NavListItem = props => {
+const channelClass = props => {
   let className = "nb-channel";
-  let channel_name_and_decorator;
   if (props.channel.id === props.currentChannelId) {
     className += " current-nb-channel";
   }
   if (props.channel.newMessages) {
     className += " nb-channel-bold";
   }
-  if (props.channel.channel_type === "dm") {
-    channel_name_and_decorator = props.fixDMName(props.channel.name);
-  } else {
-    channel_name_and_decorator = "#" + props.channel.name;
-  }
-  return (
-    <li className={ className }
-      onClick={ props.fetchChannel(props.channel.id) }>
-      { channel_name_and_decorator }
-    </li>
-  );
+  return className;
 };
+
+const channelText = props => {
+  if (props.channel.channel_type === "dm") {
+    return props.fixDMName(props.channel.name);
+  } else {
+    return "#" + props.channel.name;
+  }
+};
+
+const NavListItem = props => (
+  <li className={ channelClass(props) }
+    onClick={ props.fetchChannel(props.channel.id) }>
+    { channelText(props) }
+  </li>
+);
 
 export default NavListItem;
