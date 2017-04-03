fd87aca67343a67da5e72597a3d74c4a045a19db
hrb90 <harrisonrbrown90@gmail.com>
2017-04-03 15:02:19 -0400

    Setup testing environment; add first test

---
+++ test/frontend/components/nav_bar/nav_list_item.test.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import NavListItem from '../../../../frontend/components/nav_bar/nav_list_item';
+import renderer from 'react-test-renderer';
+
+const mockNameFix = jest.fn();
+mockNameFix.mockReturnValue("Fixed!");
+
+const mockFetchChannel = id => jest.fn().mockReturnValue("Fetched!");
+
+const basicChannel = {
+      id: 2,
+      name: "channel",
+      newMessages: false,
+      channel_type: "channel"
+    };
+
+const newMessagesChannel = {
+      id: 2,
+      name: "channel",
+      newMessages: true,
+      channel_type: "channel"
+    };
+
+const dmChannel ={
+      id: 2,
+      name: "channel",
+      newMessages: false,
+      channel_type: "dm"
+    };
+
+describe('NavListItem', () => {
+  it('renders basic props correctly', () => {
+    const tree = renderer.create(
+      <NavListItem channel={ basicChannel } currentChannelId={ 1 }
+        fixDMName={ mockNameFix } fetchChannel={ mockFetchChannel } />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders new messages correctly', () => {
+    const tree = renderer.create(
+      <NavListItem channel={ newMessagesChannel } currentChannelId={ 1 }
+        fixDMName={ mockNameFix } fetchChannel={ mockFetchChannel } />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the current channel correctly', () => {
+    const tree = renderer.create(
+      <NavListItem channel={ basicChannel } currentChannelId={ 2 }
+        fixDMName={ mockNameFix } fetchChannel={ mockFetchChannel } />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('calls fixDMName when passed a DM channel', () => {
+    renderer.create(
+      <NavListItem channel={ dmChannel } currentChannelId={ 1 }
+        fixDMName={ mockNameFix } fetchChannel={ mockFetchChannel } />
+    );
+    expect(mockNameFix.mock.calls.length).toBe(1);
+  });
+});
