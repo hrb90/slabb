9d0dc178e1b23dcf862df488d4e822c904b9b7d6
hrb90 <harrisonrbrown90@gmail.com>
2017-04-24 13:42:01 -0400

    Add day breaks

--- frontend/components/channels/channel_messages.jsx
+++ frontend/components/channels/channel_messages.jsx
@@ -1,87 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { updateMessage,
-  deleteMessage,
-  beginEditMessage,
-  endEditMessage } from '../../actions/message_actions';
-import Message from './messages/message';
-import MessageForm from './message_form';
-
-const MAX_GROUP_TIME_DIFF = 30 * 60 * 1000; // 30 minutes in milliseconds
+import DayContainer from './day_container';
 
 const mapStateToProps = ({ currentChannel, session }) => ({
-  messages: currentChannel.messages,
-  currentUserId: session.currentUser.id
+  messages: currentChannel.messages
 });
 
-const mapDispatchToProps = dispatch => ({
-  updateMessage: message => dispatch(updateMessage(message)),
-  beginEditMessage: messageId => () => dispatch(beginEditMessage(messageId)),
-  endEditMessage: messageId => () => dispatch(endEditMessage(messageId)),
-  deleteMessage: messageId => () => dispatch(deleteMessage(messageId))
-});
-
-class ChannelMessages extends React.Component {
-  constructor(props) {
-    super(props);
-    this.editCallback = this.editCallback.bind(this);
-    this.pickFormOrMessage = this.pickFormOrMessage.bind(this);
-  }
-
-  editCallback(message) {
-    this.props.updateMessage(message)
-      .then(this.props.endEditMessage(message.id));
-  }
-
-  firstMsgIds(msgs) {
-    let messages = msgs.slice(0).reverse();
-    let firstMsgIds = [];
-    if (messages.length > 0) {
-      firstMsgIds.push(messages[0].id);
-      let currentAuthorId = messages[0].author.id;
-      let currentCreatedDate = new Date(messages[0].created_at);
-      messages.forEach(message => {
-        let msgDate = new Date(message.created_at);
-        if (message.author.id !== currentAuthorId ||
-            msgDate - currentCreatedDate > MAX_GROUP_TIME_DIFF) {
-          firstMsgIds.push(message.id);
-          currentAuthorId = message.author.id;
-          currentCreatedDate = msgDate;
-        }
-      });
-    }
-    return firstMsgIds;
-  }
-
-  pickFormOrMessage(firstMsgIds) {
-    return (msg) => {
-      if (msg.isEditing) {
-        return (
-          <MessageForm key={ "edit" + msg.id }
-            messageId={ msg.id }
-            content={ msg.content }
-            submitCallback={ this.editCallback }
-            blurCallback={ this.props.endEditMessage(msg.id) }
-            focusOnMount={ true }/>);
-      } else {
-          return (<Message message={ msg }
-            key={ "message" + msg.id }
-            isFirst={ firstMsgIds.includes(msg.id) }
-            currentUserId={ this.props.currentUserId }
-            beginEditMessage={ this.props.beginEditMessage(msg.id) }
-            deleteMessage={ this.props.deleteMessage(msg.id) } />);
-      }
-    };
-  }
-
-  render() {
-    let firstMsgIds = this.firstMsgIds(this.props.messages);
-    return (
-      <div className="msgs-container">
-        { this.props.messages.map(this.pickFormOrMessage(firstMsgIds)) }
-      </div>
-    );
-  }
+const dateString = d => {
+  let dateD = new Date(d);
+  return dateD.toLocaleDateString();
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChannelMessages);
+
+const splitIntoDates = (msgs) => {
+  let groups = [{msgs: [], date: null}];
+  if (msgs.length > 0) {
+    let currDate = dateString(msgs[0].created_at);
+    groups[0].date = dateString(msgs[0].created_at);
+    msgs.forEach(msg => {
+      if (dateString(msg.created_at) != currDate) {
+        groups.push({msgs: [msg], date: msg.created_at});
+        currDate = dateString(msg.created_at);
+      } else {
+        groups[groups.length - 1].msgs.push(msg);
+      }
+    })
+  }
+  return groups;
+}
+
+const ChannelMessages = props => (
+  <div className="msgs-container">
+    { splitIntoDates(props.messages).map((dateGroup, idx) =>
+      (<DayContainer messages={ dateGroup.msgs } date={ dateGroup.date } key={ idx }/>)) }
+  </div>
+)
+
+export default connect(mapStateToProps)(ChannelMessages);
---
+++ frontend/components/channels/day_container.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateMessage,
+  deleteMessage,
+  beginEditMessage,
+  endEditMessage } from '../../actions/message_actions';
+import Message from './messages/message';
+import MessageForm from './message_form';
+
+const MAX_GROUP_TIME_DIFF = 30 * 60 * 1000; // 30 minutes in milliseconds
+
+const mapStateToProps = ({ session }) => ({
+  currentUserId: session.currentUser.id
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateMessage: message => dispatch(updateMessage(message)),
+  beginEditMessage: messageId => () => dispatch(beginEditMessage(messageId)),
+  endEditMessage: messageId => () => dispatch(endEditMessage(messageId)),
+  deleteMessage: messageId => () => dispatch(deleteMessage(messageId))
+});
+
+class ChannelMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.editCallback = this.editCallback.bind(this);
+    this.pickFormOrMessage = this.pickFormOrMessage.bind(this);
+  }
+
+  editCallback(message) {
+    this.props.updateMessage(message)
+      .then(this.props.endEditMessage(message.id));
+  }
+
+  firstMsgIds(msgs) {
+    let messages = msgs.slice(0).reverse();
+    let firstMsgIds = [];
+    if (messages.length > 0) {
+      firstMsgIds.push(messages[0].id);
+      let currentAuthorId = messages[0].author.id;
+      let currentCreatedDate = new Date(messages[0].created_at);
+      messages.forEach(message => {
+        let msgDate = new Date(message.created_at);
+        if (message.author.id !== currentAuthorId ||
+            msgDate - currentCreatedDate > MAX_GROUP_TIME_DIFF) {
+          firstMsgIds.push(message.id);
+          currentAuthorId = message.author.id;
+          currentCreatedDate = msgDate;
+        }
+      });
+    }
+    return firstMsgIds;
+  }
+
+  pickFormOrMessage(firstMsgIds) {
+    return (msg) => {
+      if (msg.isEditing) {
+        return (
+          <MessageForm key={ "edit" + msg.id }
+            messageId={ msg.id }
+            content={ msg.content }
+            submitCallback={ this.editCallback }
+            blurCallback={ this.props.endEditMessage(msg.id) }
+            focusOnMount={ true }/>);
+      } else {
+          return (<Message message={ msg }
+            key={ "message" + msg.id }
+            isFirst={ firstMsgIds.includes(msg.id) }
+            currentUserId={ this.props.currentUserId }
+            beginEditMessage={ this.props.beginEditMessage(msg.id) }
+            deleteMessage={ this.props.deleteMessage(msg.id) } />);
+      }
+    };
+  }
+
+  render() {
+    let firstMsgIds = this.firstMsgIds(this.props.messages);
+    // remember, this is flex-reversed, so the header comes at the end (:
+    return (
+      <div className="day-container">
+        { this.props.messages.map(this.pickFormOrMessage(firstMsgIds)) }
+        <span className="day-header">{ this.props.date }</span>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelMessages);
